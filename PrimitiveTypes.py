17e555a235a4ae74ad5098b224f82390cb7e7ae2
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-02-07 19:42:56 -0600

    started python repo / primitive types

---
+++ PrimitiveTypes.py
@@ -0,0 +1,17 @@
+# python contains numbers and floats as opposed to javascript where every number is a number
+
+add = 2 + 2
+print(add)
+
+subtract = 50 - 10
+print(subtract)
+
+# division will always return a floating point / use // to floor the number returned from division
+divide = 10 / 4
+print(divide)
+
+floorDivide = 10 // 4
+print(floorDivide)
+
+remainder = 3 % 2
+print(remainder)
