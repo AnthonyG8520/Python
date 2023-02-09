33b326731be022053d69d9393387769c4adbb0a7
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-02-09 00:24:23 -0600

    added if else to conditionals file / starting match statements

---
+++ Notes/ControlFlow/Conditionals.py
@@ -0,0 +1,26 @@
+# -------------if statements-------------
+
+# the input keyword shown is used to take input from the console
+# x = int(input('Please input a number:'))
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Your number was zero')
+# elif x == 1:
+#     print('Your number was one')
+# else:
+#     print('Your number was: ' + str(x))
+
+# ------------------Match Statements--------------------
+
+
+
+
+
+
+
+
+
+
