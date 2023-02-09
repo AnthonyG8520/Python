d151bc63792bd117b9e2bfb981fb32b64aa222b0
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-02-09 01:16:57 -0600

    added match statement

--- Notes/ControlFlow/Conditionals.py
+++ Notes/ControlFlow/Conditionals.py
@@ -14,12 +14,27 @@
 #     print('Your number was: ' + str(x))
 
 # ------------------Match Statements--------------------
+# A match statement takes an expression and compares its value to successive patterns given as one or more case blocks
+# similar to switch case statement
 
+def httpError(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the internet"
+# Note the last block: the “variable name” _ acts as a wildcard and
+# never fails to match. If no case matches, none of the branches is executed.
 
+print(httpError(404))
 
-
-
-
+# you can combine literals is a single case statement using a single pipe '|' known as "or"
+# case 401 | 403 | 404:
+#     return "not allowed"
 
 
 
