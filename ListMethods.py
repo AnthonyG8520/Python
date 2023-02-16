521c7524409675fdf1c69ad56e92c19531921bad
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-02-16 01:29:05 -0600

    more list notes

--- ListMethods.py
+++ ListMethods.py
@@ -22,9 +22,9 @@ print(fruits)
 # in addition to pop() there is popleft() which will remove an element from the front of the list
 # however you need to import the collection.deque package and use different syntax for list
 # this method is not efficient due to speed
-# from collections import deque
-# queue = deque(["Eric", "John", "Michael"])
-# queue.popleft()
+    # from collections import deque
+    # queue = deque(["Eric", "John", "Michael"])
+    # queue.popleft()
 
 # removes the first item with the specified value
 fruits.remove('orange')
@@ -43,10 +43,49 @@ print(fruits)
 fruits.clear()
 print(fruits)
 
+# --------------------List Comprehension---------------------
+# List comprehensions provide a concise way to create lists.
+# Common applications are to make new lists where each element is the result of some operations
+# applied to each member of another sequence or iterable, or to create a subsequence
+# of those elements that satisfy a certain condition.
 
+squares = []
+# for x in range(10):
+#     squares.append(x**2)
+# print(squares)
 
+# instead we could use
+squares = [x**2 for x in range(10)]
+print(squares)
 
+# list comprehension can contain multiple for clauses as well as if clauses
 
+combs = []
+# for x in [1, 2, 3]:
+#     for y in [3, 1, 4]:
+#         if x != y:
+#             combs.append((x, y))
+# print(combs)
+
+# instead we could use
+combs = [(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y]
+print(combs)
+# If the expression is a tuple (e.g. the (x, y) in the previous example), it must be parenthesized.
+
+# we could use list comprehension to quickly double all the numbers in a list
+nums = [-4, -2, 0, 2, 4]
+doubled = [x*2 for x in nums]
+print(doubled)
+
+# we could even use it to filter out certain values
+positives = [x for x in nums if x >= 0]
+print(positives)
+
+# also used for running methods on each element
+vegetables = ['   carrot  ', '    brocolli   ']
+
+vegetables = [vegetable.strip() for vegetable in vegetables]
+print(vegetables)
 
 
 
