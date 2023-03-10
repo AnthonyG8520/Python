bf27de8ff9be0f44bdb597a2e4850afc65fe2643
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-03-10 00:35:17 -0600

    scratch work

--- ScratchWork.py
+++ ScratchWork.py
@@ -12,19 +12,26 @@ def filter_out_strings(lis):
     int_list = [num for num in lis if isinstance(num, int)]
     return int_list
 
+
 def fizz_buzz(num):
-
-
-
-
-
+    i = 1
+    while i <= num:
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+        i += 1
 
 
 print(disem_vowel("Hello"))
 
 print(filter_out_strings([1, 2, 'a', 'b']))
 
-
+fizz_buzz(100)
 
 
 
