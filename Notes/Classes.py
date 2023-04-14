0466877403fcfc16b2d4a233f4ee866237936276
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-04-13 20:13:28 -0500

    started more classes notes

--- Notes/Classes.py
+++ Notes/Classes.py
@@ -1,10 +1,23 @@
 # A Class is like an object constructor, or a "blueprint" for creating objects.
+# In practice, the statements inside a class definition will usually be function definitions,
+# but other statements are allowed, and sometimes useful — we’ll come back to this later.
+
 
 class Person:
+    say_hello = "Hello"
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-ant = Person('anthony', 23)
+# Attribute references use the standard syntax used for all attribute references in
+# Python: obj.name. Valid attribute names are all the names that were
+# in the class’s namespace when the class object was created.
 
-print('My name is ' + ant.name + ' and I\'m ' + str(ant.age))
+
+print(Person.say_hello)  # This is an attribute reference
+
+ant = Person('anthony', 23)  # This is an instantiation of a class
+
+# obj = Person()  # This is also a class instantiation that is valid when no init definition is present
+
